1001058445fc34948d4983afff7100c268c7f305
Vinicius Nunes <vnunes9@gmail.com>
2026-02-17 11:31:18 -0300

    Iniciando automação do teste de consulta de pedido aprovado

---
+++ playwright/e2e/pedidos.spec.ts
@@ -0,0 +1,17 @@
+import { test, expect } from '@playwright/test'
+
+test('COnsultar um pedido aprovado', async ({ page }) => {
+  await page.goto('http://localhost:5173/')
+
+  await expect(page.getByTestId('hero-section').getByRole('heading')).toContainText('Velô Sprint')
+
+  await page.getByRole('link', { name: 'Consultar Pedido' }).click()
+  await expect(page.getByRole('heading')).toContainText('Consultar Pedido')
+
+  await page.getByTestId('search-order-id').fill('VLO-BVOH08')
+  await page.getByTestId('search-order-button').click()
+  
+  await expect(page.getByTestId('order-result-id')).toContainText('VLO-BVOH08')
+  await expect(page.getByTestId('order-result-status')).toContainText('APROVADO')
+  await expect(page.getByTestId('order-result-VLO-BVOH08')).toContainText('vinicius@teste.com')
+})
